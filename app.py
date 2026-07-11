db65b1d84af83e528acef418aca66cedb3d4bd77
EDSON_FÉLIX <edsonfelix@gmail.com>
2026-07-11 10:34:39 -0300

    criando rotas e criando os arquivos login e cadastro.html

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('cadastro')
+def cadastro():
+    pass
+
+@app.route('/login')
+def login():
+    pass
+
 
 if __name__ == "__main__":
     app.run(debug=True)
