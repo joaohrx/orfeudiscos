49ccb7b97e67b2aa789c74ad1e8cabdc4cf7ecb5
Wesley <wesley.kargery@escolar.ifrn.edu.br>
2026-07-11 23:17:41 -0300

    feat: adicionar nova tabela
    adiciona a tabela usuário ao banco de dados

--- banco.py
+++ banco.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 
 from sqlalchemy.orm import sessionmaker, declarative_base
 
@@ -7,6 +7,18 @@ Session = sessionmaker(bind=db)
 session = Session()
 
 Base = declarative_base()
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    nome = Column("nome", String)
+    email = Column("email", String)
+    senha = Column("senha", String)
+    
+    def __init__(self, nome, email, senha):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
     
     
 Base.metadata.create_all(bind=db)
